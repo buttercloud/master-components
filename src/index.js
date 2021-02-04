704cf02a8f2c8996c61b92c521f1951247bc77d7
ibrahim-mousa <ibrahimmousa1996@hotmail.com>
2021-02-04 15:17:47 +0200

    Bundle the package using rollup into bundle.js and update package.json main to use it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // @flow
-import Text from './components/Text';
-import Image from './components/Image';
-import Products from './components/Products';
+import Text from '@buttercloud/theme-builder-master-components/src/components/Text';
+import Image from '@buttercloud/theme-builder-master-components/src/components/Image';
+import Products from '@buttercloud/theme-builder-master-components/src/components/Products';
 
 export {
   Text,
