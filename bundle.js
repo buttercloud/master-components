c9787858255e0b86004a9f21d37ef0b53a8b706c
ibrahim-mousa <ibrahimmousa1996@hotmail.com>
2021-02-04 19:11:40 +0200

    Try another command for rollup

--- bundle.js
+++ bundle.js
@@ -1,34 +1,36 @@
-'use strict';
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@buttercloud/theme-builder-master-components/src/components/Text'), require('@buttercloud/theme-builder-master-components/src/components/Image'), require('@buttercloud/theme-builder-master-components/src/components/Products')) :
+  typeof define === 'function' && define.amd ? define(['exports', '@buttercloud/theme-builder-master-components/src/components/Text', '@buttercloud/theme-builder-master-components/src/components/Image', '@buttercloud/theme-builder-master-components/src/components/Products'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.myBundle = {}, global.Text, global.Image, global.Products));
+}(this, (function (exports, Text, Image, Products) { 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-var Text = require('@buttercloud/theme-builder-master-components/src/components/Text');
-var Image = require('@buttercloud/theme-builder-master-components/src/components/Image');
-var Products = require('@buttercloud/theme-builder-master-components/src/components/Products');
-
-function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
-
-var Text__default = /*#__PURE__*/_interopDefaultLegacy(Text);
-var Image__default = /*#__PURE__*/_interopDefaultLegacy(Image);
-var Products__default = /*#__PURE__*/_interopDefaultLegacy(Products);
+  var Text__default = /*#__PURE__*/_interopDefaultLegacy(Text);
+  var Image__default = /*#__PURE__*/_interopDefaultLegacy(Image);
+  var Products__default = /*#__PURE__*/_interopDefaultLegacy(Products);
 
 
 
-Object.defineProperty(exports, 'Text', {
-  enumerable: true,
-  get: function () {
-    return Text__default['default'];
-  }
-});
-Object.defineProperty(exports, 'Image', {
-  enumerable: true,
-  get: function () {
-    return Image__default['default'];
-  }
-});
-Object.defineProperty(exports, 'Products', {
-  enumerable: true,
-  get: function () {
-    return Products__default['default'];
-  }
-});
+  Object.defineProperty(exports, 'Text', {
+    enumerable: true,
+    get: function () {
+      return Text__default['default'];
+    }
+  });
+  Object.defineProperty(exports, 'Image', {
+    enumerable: true,
+    get: function () {
+      return Image__default['default'];
+    }
+  });
+  Object.defineProperty(exports, 'Products', {
+    enumerable: true,
+    get: function () {
+      return Products__default['default'];
+    }
+  });
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
