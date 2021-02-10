989918887e8c02bc15e28fa70cda7832e827c19e
ibrahim-mousa <ibrahimmousa1996@hotmail.com>
2021-02-10 10:29:06 +0200

    Remove old config setup and use create-react-app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-// @flow
-import Text from '@buttercloud/theme-builder-master-components/src/components/Text';
-import Image from '@buttercloud/theme-builder-master-components/src/components/Image';
-import Products from '@buttercloud/theme-builder-master-components/src/components/Products';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export {
-  Text,
-  Image,
-  Products,
-};
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
