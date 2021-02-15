f5145108d8edd67041f383c5b08826d07b59eadd
ibrahim-mousa <ibrahimmousa1996@hotmail.com>
2021-02-15 23:16:30 +0200

    - Add styled-components. - Create an app using create-react-app called playground to test the library on

---
+++ playground/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Image, Header } from '@buttercloud/master-components';
+
+const App = () => {
+  return (
+    <div>
+      <Image />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
