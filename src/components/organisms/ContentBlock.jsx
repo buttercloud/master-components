a9015317205d04e43bfc522667db92ef92d1f482
ibrahim-mousa <ibrahimmousa1996@hotmail.com>
2021-03-04 08:44:54 +0200

    Add a ContentBlock component with a centered title, image on one side and text on the other

---
+++ src/components/organisms/ContentBlock.jsx
@@ -0,0 +1,149 @@
+// @flow
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+const ImageAndContentContainer = styled.div`
+  display: flex;
+`;
+const ImageContainer = styled.div`
+  display: flex;
+  flex: 0.35;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+const ContentContainer = styled.div`
+  display: flex;
+  flex: 0.65;
+  padding: 5px;
+`;
+const Image = styled.img`
+  width: 100%;
+  border-radius: 5px;
+`;
+const Title = styled.p`
+  font-size: 1.7rem;
+  font-weight: bold;
+  margin-bottom: 0px;
+  color: ${(props) => props.color};
+`;
+const Subtitle = styled.p`
+  font-size: 1rem;
+  color: ${(props) => props.color};
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+const Content = styled.p`
+  font-size: ${(props) => props.fontSize}rem;
+  color: ${(props) => props.color};
+`;
+const Separator = styled.hr`
+  width: 10%;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  border: 2px solid ${(props) => props.color};
+  border-radius: 5px;
+`;
+
+/**
+* Content block. centered title, image on one side and text on the other
+*/
+const ContentBlock = (props) => {
+  const {
+    imgUrl,
+    title,
+    subtitle,
+    content,
+    contentColor,
+    contentFontSize,
+    titleColor,
+    subtitleColor,
+    separatorColor,
+  } = props;
+
+  return (
+    <Container>
+      <TitleContainer>
+        <Title color={titleColor}>
+          {title}
+        </Title>
+        <Subtitle color={subtitleColor}>
+          {subtitle}
+        </Subtitle>
+        <Separator color={separatorColor} />
+      </TitleContainer>
+      <ImageAndContentContainer>
+        <ImageContainer>
+          <Image src={imgUrl} alt="" />
+        </ImageContainer>
+        <ContentContainer>
+          <Content fontSize={contentFontSize} color={contentColor}>
+            {content}
+          </Content>
+        </ContentContainer>
+      </ImageAndContentContainer>
+    </Container>
+  );
+};
+
+ContentBlock.defaultProps = {
+  title: '',
+  subtitle: '',
+  contentColor: 'gray',
+  contentFontSize: 1.1,
+  titleColor: 'black',
+  subtitleColor: 'gray',
+  separatorColor: 'orange',
+};
+
+ContentBlock.propTypes = {
+  /**
+  * Image url.
+  */
+  imgUrl: PropTypes.string.isRequired,
+  /**
+  * The main title.
+  */
+  title: PropTypes.string,
+  /**
+  * Secondary title.
+  */
+  subtitle: PropTypes.string,
+  /**
+  * Content block.
+  */
+  content: PropTypes.string.isRequired,
+  /**
+  * Content color.
+  */
+  contentColor: PropTypes.string,
+  /**
+  * Content font size.
+  */
+  contentFontSize: PropTypes.number,
+  /**
+  * Main title color.
+  */
+  titleColor: PropTypes.string,
+  /**
+  * Secondary title color.
+  */
+  subtitleColor: PropTypes.string,
+  /**
+  * Separator color.
+  */
+  separatorColor: PropTypes.string,
+};
+
+export default ContentBlock;
