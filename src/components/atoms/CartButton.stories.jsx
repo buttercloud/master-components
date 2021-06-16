4a4ec14c5aa21efb0296a9f46df00d2105b3f0e1
ibrahim-mousa <ibrahimmousa1996@hotmail.com>
2021-06-16 15:10:59 +0300

    - Add an AddToCartButton atom. - Add a CartButton atom with notifications badge. - Add a controlled TextareaWithLabel input molecule component.

---
+++ src/components/atoms/CartButton.stories.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CartButton from './CartButton';
+
+export default {
+  title: 'Atoms/Buttons/Cart',
+  component: CartButton,
+  argTypes: {
+    iconColor: { control: 'color' },
+    badgeColor: { control: 'color' },
+    fontColor: { control: 'color' },
+    badgeNumber: { control: 'number' },
+    badgePosition: { control: 'radio' },
+  },
+};
+
+const Template = (args) => <CartButton {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  iconColor: 'palevioletred',
+  badgeColor: 'palevioletred',
+  badgeNumber: 5,
+  badgePosition: 'bottom-left',
+};
+
+Primary.storyName = 'Cart';
---
+++ src/components/molecules/TextareaWithLabel.stories.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import TextareaWithLabel from './TextareaWithLabel';
+
+export default {
+  title: 'molecules/Inputs/TextareaWithLabel',
+  component: TextareaWithLabel,
+  argTypes: {
+    labelColor: {
+      control: 'color',
+    },
+  },
+};
+
+const Template = (args) => {
+  const [value, setValue] = useState();
+
+  return (
+    <TextareaWithLabel
+      {...args}
+      value={value}
+      setValue={setValue}
+    />
+  );
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  label: 'Lorem Ipsum',
+  labelColor: 'palevioletred',
+  value: '',
+  setValue: (newValue) => console.log('value: ', newValue),
+};
+
+Primary.storyName = 'TextareaWithLabel';
