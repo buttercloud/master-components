1a3b6ad6d9055f7f73c2c0bf5d45ab1ba2958725
Ibrahim Mousa <ibrahimmousa1996@hotmail.com>
2021-11-11 14:57:46 +0200

    Configure storybook to handle tailwindcss

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 // import '../src/index.css';
+import '../src/assets/css/tailwind.css';
+import '../src/assets/css/icons.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
